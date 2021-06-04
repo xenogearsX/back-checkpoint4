82162d5211be722ece3a0471e2a7d0170169140e
xenogearsX <xenogears@hotmail.fr>
2021-06-04 17:21:09 +0200

    add password encrypt before saving in bdd + route for signin and generate token if signin ok

--- routes/Account.js
+++ routes/Account.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const connection = require('../config')
 const router = express.Router()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const {
     email,
     password,
@@ -14,11 +17,14 @@ router.post('/', (req, res) => {
     city,
     country
   } = req.body
+  const salt = await bcrypt.genSalt()
+  const hashedPassword = await bcrypt.hash(password, salt)
+
   connection.query(
     'INSERT INTO account (email, password, firstname, lastname, streetnumber, streetname, zipcode, city, country) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)',
     [
       email,
-      password,
+      hashedPassword,
       firstname,
       lastname,
       streetnumber,
@@ -33,6 +39,33 @@ router.post('/', (req, res) => {
         res.status(500).send('Error saving account')
       }
       res.status(200).send('Successfully saved account')
+    }
+  )
+})
+
+router.post('/signin', (req, res) => {
+  connection.query(
+    'SELECT * from account WHERE email= ? ',
+    [req.body.email],
+    (err, result) => {
+      if (err) {
+        res.status(400).send('Impossible car :' + err)
+      } else {
+        if (
+          req.body.email === result[0].email &&
+          bcrypt.compareSync(req.body.password, result[0].password)
+        ) {
+          const tokenUserinfo = {
+            email: req.body.email
+          }
+          const token = jwt.sign(tokenUserinfo, process.env.JWT_SECRET)
+          res.header('Access-Control-Expose-Headers', 'x-access-token')
+          res.set('x-access-token', token)
+          res.status(200).send({ details: 'user connected' })
+        } else {
+          res.status(201).send('Failed')
+        }
+      }
     }
   )
 })
