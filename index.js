390f09737cf302d442bb0b651735acb2ae6019f2
xenogearsX <xenogears@hotmail.fr>
2021-07-19 12:26:59 +0200

    create password route

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/account', routes.account)
 app.use('/order', routes.order)
+app.use('/password', routes.password)
 app.use('/products', routes.products)
 app.use('/types', routes.types)
 app.use('/typegroups', routes.typegroups)
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const order = require('./Order')
 const products = require('./Products')
 const types = require('./Types')
 const typegroups = require('./Typegroups')
+const password = require('./Password')
 
 module.exports = {
   account,
   order,
+  password,
   products,
   types,
   typegroups
